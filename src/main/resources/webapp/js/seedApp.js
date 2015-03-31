9531b590320111b1710109211cd8abf9e26c21bd
Johannes Brodwall <johannes@brodwall.com>
2015-03-31 10:43:57 +0200

    Fixed conventions and warnings

--- src/main/resources/webapp/js/seedApp.js
+++ src/main/resources/webapp/js/seedApp.js
@@ -1,34 +1,33 @@
 var ajax = {
-	get: function(localUrl) {
-		return $.get('/seedapp/' + localUrl);
-	},
-	post: function(localUrl, object) {
-		return $.ajax({
-	        url: '/seedapp/' + localUrl,
-	        type: 'POST',
-	        data: JSON.stringify(object),
-	        contentType: "application/json; charset=utf-8"
-	    });
-	}
+  get : function(localUrl) {
+    return $.get('/seedapp/' + localUrl);
+  },
+  post : function(localUrl, object) {
+    return $.ajax({
+      url : '/seedapp/' + localUrl,
+      type : 'POST',
+      data : JSON.stringify(object),
+      contentType : "application/json; charset=utf-8"
+    });
+  }
 };
 
-
 var orderRepository = {
-  list: function() {
+  list : function() {
     return ajax.get('api/orders');
   },
-  
-  save: function(order) {
+
+  save : function(order) {
     return ajax.post('api/orders', order);
   }
 };
 
 var productRepository = {
-  list: function() {
+  list : function() {
     return ajax.get('api/products');
   },
-  
-  save: function(products) {
+
+  save : function(products) {
     return ajax.post('api/products', products);
   }
 };
