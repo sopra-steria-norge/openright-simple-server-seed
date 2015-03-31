17c3a6f11f85a642baeb20ccaed90faf400a3451
Johannes Brodwall <johannes@brodwall.com>
2015-03-31 10:44:10 +0200

    Added products to order edit

--- src/main/java/net/openright/simpleserverseed/domain/orders/OrdersApiController.java
+++ src/main/java/net/openright/simpleserverseed/domain/orders/OrdersApiController.java
@@ -1,4 +1,5 @@
 package net.openright.simpleserverseed.domain.orders;
+
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -12,33 +13,33 @@ import org.json.JSONObject;
 
 public class OrdersApiController implements JsonController {
 
-	private OrdersRepository repository;
+    private OrdersRepository repository;
 
-	public OrdersApiController(PgsqlDatabase database) {
-		this.repository = new OrdersRepository(database);
-	}
+    public OrdersApiController(PgsqlDatabase database) {
+        this.repository = new OrdersRepository(database);
+    }
 
-	@Override
-	public JSONObject getJSON(HttpServletRequest req) {
-		return new JSONObject()
-			.put("orders", mapToJSON(repository.list(), this::toJSON));
-	}
+    @Override
+    public JSONObject getJSON(HttpServletRequest req) {
+        return new JSONObject()
+            .put("orders", mapToJSON(repository.list(), this::toJSON));
+    }
 
-	@Override
-	public void postJSON(JSONObject jsonObject) {
-		repository.insert(toOrder(jsonObject));
-	}
+    @Override
+    public void postJSON(JSONObject jsonObject) {
+        repository.insert(toOrder(jsonObject));
+    }
 
-	private Order toOrder(JSONObject jsonObject) {
-		Order order = new Order(jsonObject.getString("title"));
-		order.setId(jsonObject.getInt("id"));
-		return order;
-	}
+    private Order toOrder(JSONObject jsonObject) {
+        Order order = new Order(jsonObject.getString("title"));
+        return order;
+    }
 
-	private JSONObject toJSON(Order order) {
-		return new JSONObject().put("id", order.getId()).put("title", order.getTitle());
-	}
-
+    private JSONObject toJSON(Order order) {
+        return new JSONObject()
+            .put("id", order.getId())
+            .put("title", order.getTitle());
+    }
 
     private <T> JSONArray mapToJSON(List<T> list, Function<T, JSONObject> mapper) {
         return new JSONArray(list.stream().map(mapper).collect(Collectors.toList()));
