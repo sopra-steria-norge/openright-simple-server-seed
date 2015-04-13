70cd13fa759ee3f4e272791df71a05ed05a03238
Ørjan Markhus Lillevik <olillevik@gmail.com>
2015-04-13 09:51:50 +0200

    Make doWithConnection private. Add javadoc to PgsqlDatabase API.

--- src/main/java/net/openright/infrastructure/db/PgsqlDatabase.java
+++ src/main/java/net/openright/infrastructure/db/PgsqlDatabase.java
@@ -15,161 +15,210 @@ import java.util.*;
 
 public class PgsqlDatabase {
 
-    private static final Logger log = LoggerFactory.getLogger(PgsqlDatabase.class);
+	private static final Logger log = LoggerFactory.getLogger(PgsqlDatabase.class);
 
-    public interface ConnectionCallback<T> {
-        T run(Connection conn);
-    }
+	public interface ConnectionCallback<T> {
+		T run(Connection conn);
+	}
 
-    public interface StatementCallback<T> {
-        T run(PreparedStatement stmt) throws SQLException;
-    }
+	public interface StatementCallback<T> {
+		T run(PreparedStatement stmt) throws SQLException;
+	}
 
-    public interface RowMapper<T> {
-        T run(Row row) throws SQLException;
-    }
+	public interface RowMapper<T> {
+		T run(Row row) throws SQLException;
+	}
 
-    public static class Row {
+	public static class Row {
 
-        private final ResultSet rs;
-        private final Map<String, Integer> columnMap = new HashMap<>();
+		private final ResultSet rs;
+		private final Map<String, Integer> columnMap = new HashMap<>();
 
-        public Row(ResultSet rs) throws SQLException {
-            this.rs = rs;
-            for (int i = 1; i < rs.getMetaData().getColumnCount() + 1; i++) {
-                String tableName = rs.getMetaData().getTableName(i);
-                String columnName = rs.getMetaData().getColumnName(i);
+		public Row(ResultSet rs) throws SQLException {
+			this.rs = rs;
+			for (int i = 1; i < rs.getMetaData().getColumnCount() + 1; i++) {
+				String tableName = rs.getMetaData().getTableName(i);
+				String columnName = rs.getMetaData().getColumnName(i);
 
-                this.columnMap.put(tableName + "." + columnName, i);
-            }
-        }
+				this.columnMap.put(tableName + "." + columnName, i);
+			}
+		}
 
-        public String getString(String string) throws SQLException {
-            return rs.getString(string);
-        }
+		public String getString(String string) throws SQLException {
+			return rs.getString(string);
+		}
 
-        public int getInt(String columnName) throws SQLException {
-            return rs.getInt(columnName);
-        }
+		public int getInt(String columnName) throws SQLException {
+			return rs.getInt(columnName);
+		}
 
-        public long getLong(String tableName, String columnName) throws SQLException {
-            return rs.getLong(getColumnIndex(tableName, columnName));
-        }
+		public long getLong(String tableName, String columnName) throws SQLException {
+			return rs.getLong(getColumnIndex(tableName, columnName));
+		}
 
-        public String getString(String tableName, String columnName) throws SQLException {
-            return rs.getString(getColumnIndex(tableName, columnName));
-        }
+		public String getString(String tableName, String columnName) throws SQLException {
+			return rs.getString(getColumnIndex(tableName, columnName));
+		}
 
-        public boolean getBoolean(String tableName, String columnName) throws SQLException {
-            return rs.getBoolean(getColumnIndex(tableName, columnName));
-        }
+		public boolean getBoolean(String tableName, String columnName) throws SQLException {
+			return rs.getBoolean(getColumnIndex(tableName, columnName));
+		}
 
-        public double getDouble(String tableName, String columnName) throws SQLException {
-            return rs.getDouble(getColumnIndex(tableName, columnName));
-        }
+		public double getDouble(String tableName, String columnName) throws SQLException {
+			return rs.getDouble(getColumnIndex(tableName, columnName));
+		}
 
-        private int getColumnIndex(String tableName, String columnName) {
-            return columnMap.get(tableName + "." + columnName);
-        }
-    }
+		private int getColumnIndex(String tableName, String columnName) {
+			return columnMap.get(tableName + "." + columnName);
+		}
+	}
 
-    private final DataSource dataSource;
-    private final static ThreadLocal<Connection> threadConnection = new ThreadLocal<>();
+	private final DataSource dataSource;
+	private final static ThreadLocal<Connection> threadConnection = new ThreadLocal<>();
 
-    public PgsqlDatabase(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+	public PgsqlDatabase(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
-    public PgsqlDatabase(String name) {
-        try {
-            this.dataSource = (DataSource) new InitialContext().lookup(name);
-        } catch (NamingException e) {
-            throw ExceptionUtil.soften(e);
-        }
-    }
-    
-    public int insert(String query, Object... parameters) {
-        return executeDbOperation(query, Arrays.asList(parameters), stmt -> {
-            ResultSet rs = stmt.executeQuery();
-            rs.next();
-            return rs.getInt("id");
-        });
-    }
+	public PgsqlDatabase(String name) {
+		try {
+			this.dataSource = (DataSource) new InitialContext().lookup(name);
+		} catch (NamingException e) {
+			throw ExceptionUtil.soften(e);
+		}
+	}
 
-    public <T> List<T> queryForList(String query, RowMapper<T> mapper, Object... params) {
-    	return executeDbOperation(query, Arrays.asList(params), stmt -> {
-            try (ResultSet rs = stmt.executeQuery()) {
-                Row row = new Row(rs);
-                List<T> result = new ArrayList<>();
-                while (rs.next()) {
-                    result.add(mapper.run(row));
-                }
-                return result;
-            }
-        });
-    }
+	/**
+	 * Insert an object into the database. Used for create operations.
+	 * 
+	 * @param query
+	 *            in SQL in PostgreSQL dialect stated as a prepared statement.
+	 * @param parameters
+	 *            values for the prepared statement.
+	 * @return the database field id from the inserted element.
+	 */
+	public int insert(String query, Object... parameters) {
+		return executeDbOperation(query, Arrays.asList(parameters), stmt -> {
+			ResultSet rs = stmt.executeQuery();
+			rs.next();
+			return rs.getInt("id");
+		});
+	}
 
-    public <T> Optional<T> queryForSingle(String query, RowMapper<T> mapper, Object... parameters) {
-    	return executeDbOperation(query, Arrays.asList(parameters), stmt -> {
-            try (ResultSet rs = stmt.executeQuery()) {
-                return mapSingleRow(rs, mapper);
-            }
-        });
-    }
+	/**
+	 * Retrieves a list of results from the database and maps it to an object.
+	 * 
+	 * @param query
+	 *            in SQL stated as a prepared statement.
+	 * @param mapper
+	 *            definition for mapping fields to the returned objects in the
+	 *            list.
+	 * @param parameters
+	 *            for the prepared statement.
+	 * @return database result mapped to list of classes.
+	 */
+	public <T> List<T> queryForList(String query, RowMapper<T> mapper, Object... parameters) {
+		return executeDbOperation(query, Arrays.asList(parameters), stmt -> {
+			try (ResultSet rs = stmt.executeQuery()) {
+				Row row = new Row(rs);
+				List<T> result = new ArrayList<>();
+				while (rs.next()) {
+					result.add(mapper.run(row));
+				}
+				return result;
+			}
+		});
+	}
 
-    public void executeOperation(String query, Object... parameters) {
-        executeDbOperation(query, Arrays.asList(parameters), PreparedStatement::executeUpdate);
-    }
+	/**
+	 * Retrieves a single result from the database and maps it to an objec.
+	 * 
+	 * @param query
+	 *            in SQL stated as a prepared statement.
+	 * @param mapper
+	 *            definition for mapping fields to the returned object.
+	 * @param parameters
+	 *            for the prepared statement.
+	 * @return database result mapped to class.
+	 */
+	public <T> Optional<T> queryForSingle(String query, RowMapper<T> mapper, Object... parameters) {
+		return executeDbOperation(query, Arrays.asList(parameters), stmt -> {
+			try (ResultSet rs = stmt.executeQuery()) {
+				return mapSingleRow(rs, mapper);
+			}
+		});
+	}
 
-    public void doInTransaction(Runnable operation) {
-        try (Connection connection = dataSource.getConnection()) {
-            threadConnection.set(connection);
-            try {
-                operation.run();
-            } finally {
-                threadConnection.set(null);
-            }
-        } catch (SQLException e) {
-            throw ExceptionUtil.soften(e);
-        }
-    }
+	/**
+	 * Update or delete operation sent to the database.
+	 * 
+	 * @param query
+	 *            in SQL stated as a prepared statement.
+	 * @param parameters
+	 *            for the prepared statement.
+	 */
+	public void executeOperation(String query, Object... parameters) {
+		executeDbOperation(query, Arrays.asList(parameters), PreparedStatement::executeUpdate);
+	}
 
-    public <T> T doWithConnection(ConnectionCallback<T> object) {
-        if (threadConnection.get() != null) {
-            return object.run(threadConnection.get());
-        }
+	/**
+	 * Create a transaction for multiple database operations like
+	 * {@link #insert(String, Object...) insert},
+	 * {@link #queryForList(String, RowMapper, Object...) queryForList},
+	 * {@link #queryForSingle(String, RowMapper, Object...) queryForSingle} or
+	 * {@link #executeOperation(String, Object...) executeOperation}
+	 *
+	 * @param operation is a functional interface to allow transaction to run in a thread.
+	 */
+	public void doInTransaction(Runnable operation) {
+		try (Connection connection = dataSource.getConnection()) {
+			threadConnection.set(connection);
+			try {
+				operation.run();
+			} finally {
+				threadConnection.set(null);
+			}
+		} catch (SQLException e) {
+			throw ExceptionUtil.soften(e);
+		}
+	}
 
-        try (Connection conn = dataSource.getConnection()) {
-            return object.run(conn);
-        } catch (SQLException e) {
-            throw ExceptionUtil.soften(e);
-        }
-    }
-    
-    private <T> T executeDbOperation(String query, Collection<Object> parameters, StatementCallback<T> statementCallback) {
-        return doWithConnection(conn -> {
-            log.info("Executing: {} with params {}", query, parameters);
-            try (PreparedStatement prepareStatement = conn.prepareStatement(query)) {
-                int index = 1;
-                for (Object object : parameters) {
-                    prepareStatement.setObject(index++, object);
-                }
+	private <T> T doWithConnection(ConnectionCallback<T> object) {
+		if (threadConnection.get() != null) {
+			return object.run(threadConnection.get());
+		}
 
-                return statementCallback.run(prepareStatement);
-            } catch (SQLException e) {
-                throw ExceptionUtil.soften(e);
-            }
-        });
-    }
-    
-    private <T> Optional<T> mapSingleRow(ResultSet rs, RowMapper<T> mapper) throws SQLException {
-        if (!rs.next()) {
-            return Optional.empty();
-        }
-        T result = mapper.run(new Row(rs));
-        if (rs.next()) {
-            throw new RuntimeException("Duplicate");
-        }
-        return Optional.of(result);
-    }
+		try (Connection conn = dataSource.getConnection()) {
+			return object.run(conn);
+		} catch (SQLException e) {
+			throw ExceptionUtil.soften(e);
+		}
+	}
+
+	private <T> T executeDbOperation(String query, Collection<Object> parameters, StatementCallback<T> statementCallback) {
+		return doWithConnection(conn -> {
+			log.info("Executing: {} with params {}", query, parameters);
+			try (PreparedStatement prepareStatement = conn.prepareStatement(query)) {
+				int index = 1;
+				for (Object object : parameters) {
+					prepareStatement.setObject(index++, object);
+				}
+
+				return statementCallback.run(prepareStatement);
+			} catch (SQLException e) {
+				throw ExceptionUtil.soften(e);
+			}
+		});
+	}
+
+	private <T> Optional<T> mapSingleRow(ResultSet rs, RowMapper<T> mapper) throws SQLException {
+		if (!rs.next()) {
+			return Optional.empty();
+		}
+		T result = mapper.run(new Row(rs));
+		if (rs.next()) {
+			throw new RuntimeException("Duplicate");
+		}
+		return Optional.of(result);
+	}
 }
