234c3d1f7698b4feb0a66aae220e38c6b1da4449
Ørjan Markhus Lillevik <olillevik@gmail.com>
2015-04-15 12:44:59 +0200

    Log response JSON from GET requests.

--- src/main/java/net/openright/infrastructure/rest/JsonGetController.java
+++ src/main/java/net/openright/infrastructure/rest/JsonGetController.java
@@ -12,42 +12,40 @@ import org.slf4j.LoggerFactory;
 
 public class JsonGetController implements GetController {
 
-    private static final Logger log = LoggerFactory.getLogger(JsonGetController.class);
+	private static final Logger log = LoggerFactory.getLogger(JsonGetController.class);
 
-    private JsonController jsonController;
+	private JsonController jsonController;
 
-    public JsonGetController(JsonController jsonController) {
-        this.jsonController = jsonController;
-    }
+	public JsonGetController(JsonController jsonController) {
+		this.jsonController = jsonController;
+	}
 
-    @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String[] parts = req.getPathInfo().split("\\/");
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		String[] parts = req.getPathInfo().split("\\/");
 
-        try {
-            if (parts.length > 2) {
-                sendResponse(resp, jsonController.getJSON(parts[2]));
-            } else {
-                sendResponse(resp, jsonController.listJSON(req));
-            }
-        } catch (RequestException e) {
-            log.warn("Invalid request {}: {}", req.getRequestURL(), e.toString());
-            resp.sendError(e.getStatusCode(), e.toString());
-        }
-    }
+		try {
+			JSONObject json = parts.length > 2 ? jsonController.getJSON(parts[2]) : jsonController.listJSON(req);
+			log.info("HTTP response body: {}", json);
+			sendResponse(resp, json);
+		} catch (RequestException e) {
+			log.warn("Invalid request {}: {}", req.getRequestURL(), e.toString());
+			resp.sendError(e.getStatusCode(), e.toString());
+		}
+	}
 
-    private void sendResponse(HttpServletResponse resp, JSONObject response) throws IOException {
-        resp.setHeader("Expires", "-1");
+	private void sendResponse(HttpServletResponse resp, JSONObject response) throws IOException {
+		resp.setHeader("Expires", "-1");
 
-        if (response == null) {
-            resp.setStatus(204);
-            return;
-        }
+		if (response == null) {
+			resp.setStatus(204);
+			return;
+		}
 
-        resp.setContentType("application/json");
-        try (Writer writer = resp.getWriter()) {
-            writer.write(response.toString());
-        }
-    }
+		resp.setContentType("application/json");
+		try (Writer writer = resp.getWriter()) {
+			writer.write(response.toString());
+		}
+	}
 
 }
